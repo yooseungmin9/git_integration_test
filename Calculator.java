6bb462b6872c02058e1a4bd41c31041fa585637e
“yooseungmin9” <2win0min@naver.com>
2025-07-25 10:22:30 +0900

    First commit

--- Calculator.java
+++ Calculator.java
@@ -2,8 +2,5 @@ public class Calculator {
 
     public static void main(String[] args) {
         System.out.println("1+1=2");
-        System.out.println("1-1=0");
-        System.out.println("2*4=8");
-        System.out.println("4/2=2");
     }
 }
