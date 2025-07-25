9c396fe24230acf1ca0acc275baa393f70130666
“yooseungmin9” <2win0min@naver.com>
2025-07-25 10:49:31 +0900

    Update6 # Conflicts: #	Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,10 +2,11 @@
 public class Calculator {
 
     public static void main(String[] args) {
-        // 로직11
+        // 로직2
         System.out.println("1+1=2");
         System.out.println("1-1=0");
         System.out.println("2*4=8");
         System.out.println("4/2=2");
+        // 완료2
     }
 }
