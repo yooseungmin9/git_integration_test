14a15c4ef76ca6a6e3f7cc8a64b9b46cdc3523fe
“yooseungmin9” <2win0min@naver.com>
2025-07-25 10:43:30 +0900

    Update3

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,8 @@
-// 작업1
+// 작업2
 public class Calculator {
 
     public static void main(String[] args) {
+        // 로직1
         System.out.println("1+1=2");
         System.out.println("1-1=0");
         System.out.println("2*4=8");
